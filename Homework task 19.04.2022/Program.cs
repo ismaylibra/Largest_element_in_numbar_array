1cb89705c3cd1391ad3b40bb1113daaabb5398b3
[ismaylibra] <[ismayiliib@code.edu.az]>
2022-04-19 18:48:23 +0400

    modified

--- Homework task 19.04.2022/Program.cs
+++ Homework task 19.04.2022/Program.cs
@@ -6,20 +6,28 @@ namespace Homework_task_19._04._2022
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Arrayin uzunlugunu daxil edin: ");
+            int numArrLength = Convert.ToInt32(Console.ReadLine());
+            int[] numArr = new int[numArrLength];
+            int i, max;
            
-            int[] numArr = { 15, 6, 9, 64, 58 };
-            
-            int max = numArr[0];
-            for (int i = 0; i < numArr.Length; i++)
+
+            max = numArr[0];
+            Console.WriteLine("Verilmis uzunluqdaki arrayin icindeki elementleri  daxil edin: ", numArrLength);
+            for ( i = 0; i < numArrLength; i++)
             {
+                Console.WriteLine(i+0 +" nomreli element: ");
+                numArr[i] = Convert.ToInt32(Console.ReadLine());
+
                 if (max < numArr[i])
                 {
 
                     max = numArr[i];
-                    Console.WriteLine("Arrayin icindeki en boyuk eded = " +numArr[i]);
+                    
 
                 }
             }
+            Console.WriteLine("Arrayin icindeki en boyuk eded = " + max);
         }
 
 
