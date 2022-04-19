dc871d8b57017be20402d59c5ca200acc47e2832
[ismaylibra] <[ismayiliib@code.edu.az]>
2022-04-19 15:58:43 +0400

    added homework

---
+++ Homework task 19.04.2022/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Homework_task_19._04._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArr = { 15, 6, 9, 64, 58 };
+            int max = numArr[0];
+            for (int i = 0; i < numArr.Length; i++)
+            {
+                if (max < numArr[i])
+                {
+
+                    max = numArr[i];
+                    Console.WriteLine(numArr[i]);
+
+                }
+            }
+        }
+
+
+    }
+}
